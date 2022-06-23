5c7b1b3f542d5e667520db1814e22b5cb3d50d9f
Giuseppe Tortorelli <peppetort@gmail.com>
2022-06-23 15:52:59 +0200

    function to get product by ean

--- lib/providers/data.dart
+++ lib/providers/data.dart
@@ -6,6 +6,7 @@ import 'package:dima21_migliore_tortorelli/models/ProductModel.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
+import 'package:collection/collection.dart';
 
 import 'authentication.dart';
 
@@ -241,5 +242,48 @@ class DataProvider with ChangeNotifier {
       }
       return false;
     }
+  }
+
+  Future<String?> getProductByEAN(String ean) async {
+    ProductModel? product =
+        loadedProducts.values.firstWhereOrNull((element) => element.ean == ean);
+
+    if (product != null) {
+      return product.id;
+    }
+
+    try {
+      List<QueryDocumentSnapshot> products =
+          (await _productsReference.where('ean', isEqualTo: ean).get()).docs;
+
+      if (products.isEmpty) {
+        return null;
+      }
+
+      Map<String, dynamic> productData =
+          products.first.data() as Map<String, dynamic>;
+
+      product = ProductModel(
+        products.first.id,
+        productData['name'],
+        productData['ean'] ?? '',
+        productData['description'],
+        '',
+        productData['category'],
+      );
+
+      loadedProducts[product.id] = product;
+      _getImageUrl(product.copyWith(image: productData['image']));
+      notifyListeners();
+      return product.id;
+    } on FirebaseException catch (e) {
+      _logger.info(e);
+      if (e.message != null) {
+        lastMessage = e.message!;
+      } else {
+        lastMessage = 'Connection error';
+      }
+    }
+    return null;
   }
 }
